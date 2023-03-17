a9d3ac6fe59aa097e34364217ef4098300ce27d1
Curtis <curtissalcedo@gmail.com>
2023-03-16 19:15:03 -0600

    Update js with cells populating

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@
 // IF WE ADD 5 IN A ROW ADD A NEW CHARACTER/CARD
 // EMOJIS
 CHARACTERS = {
-  '1': 'J', // JACK / value = 10 💲
-  '2': 'Q', // QUEEN / value = 10 💵
-  '3': 'K', // KING / value = 10 👑
-  '4': 'A', // ACE / value = 20 💎
-  '5': 'X', // KOMODO DRAGON / value = 50 🐉
+  1: '💲', // JACK / value = 10 
+  2: '💵', // QUEEN / value = 10 
+  3: '👑', // KING / value = 10 
+  4: '💎', // ACE / value = 20 
+  5: '🐉', // KOMODO DRAGON / value = 50 
   // JOKER, CARD SUITS
 }
 
@@ -18,6 +18,8 @@ CHARACTERS = {
 /*----- state variables -----*/
 // USE ID'S TO TARGET SPECIFIC CELLS
 let winArray = ['','','','','','','','','','','','','','','',];
+let renderArray;
+let checkWinArray;
 
 // REPRESENTS THE 15 CELLS THAT HOLD VALUES TO DETERMINE WINNER
 // **MAY CHANGE NAME
@@ -56,7 +58,9 @@ const winningCombos = [
 // NAME OF THE SLOT MACHINE CELLS
 const screenEls = document.querySelectorAll('cells');
 const spinButtonEl = document.getElementById('spin-button');
-const cellContainer = document.querySelector('container')
+const cellContainer = document.querySelector('container');
+const divEls = document.querySelectorAll('div');
+const messageEl = document.querySelector('h2')
 
 
 /*----- functions -----*/
@@ -64,14 +68,16 @@ const cellContainer = document.querySelector('container')
 initialize();
 
 function initialize() {
-  popUp();
+  // popUp();
   // gameBoard;
+  let winArray;
   let winTotal = 0;
   let balance = 1000;
-  renderMessage();
+  // spinReel();
   renderScreen();
-  betLogic();
   checkWinner();
+  renderMessage();
+  // betLogic();
   // USED FOR FREE SPINS IF POSSIBLE
   // countSpins();
 };
@@ -85,50 +91,145 @@ function popUp () {
   // REPORT THE WINNINGS BELOW "WIN TOTAL" AREA
 function renderMessage() {
   // START UP ANIMATION
-
-  betLogic();
-  
+  // betLogic();
   function renderStartMessage() {}
-
 };
   
   // TBD BUT I THOUGHT IT MAY BE NEEDED FOR ADDED FEATURES
-function betLogic() {
-  winArray = Array.from({length: 15}, () =>Math.floor(Math.random() * 5) + 1);
-};
-betLogic();
+// function betLogic() {
+//   winArray = Array.from({length: 15}, () =>Math.floor(Math.random() * 5) + 1);
+// };
+
 // CHECK THE RESULTS THAT END UP IN screenEls
 // COMPARE AGAINST THE cellArray and winningCombos FOR WINNERS
 // LOOK UP TO HIGHLIGHT WINNERS
-function checkWinner() {
-  
-  for (let i = 0; i < winArray.length; i++) {
-    const combo = winArray[i]
-      const a = winArray[combo[0]];
-      const b = winArray[combo[1]];
-      const c = winArray[combo[2]];
-      const d = winArray[combo[3]];
-      const e = winArray[combo[4]];
-      if (a !== '' && a === b && b === c && c === d & d === e) {
-        
-      }
-  }
-};
 
-// POSSIBLY USED TO RENDER IMAGES THAT HAVE SPECIAL EFFECTS
+
+// 
+function spinReel() {
+  winArray = Array.from({length: 15}, () =>Math.floor(Math.random() * 5) + 1);
+  console.log(winArray)
+  return winArray;
+}
+
 // POSSIBLY USED TO RESET THE screen
 function renderScreen() {
-  // spinButtonEl.addEventListener('click', spinReel)
   spinButtonEl.addEventListener('click', function(){
-    
+    spinReel(); // NEED TO CALL SPINREEL IN RENDER TO MAKE SURE THE NUMBERS ARE RESET WHEN BUTTON IS PRESSED
+    console.log(winArray)
+    const cells = document.querySelectorAll('div');
+    winArray.forEach(function(cell, index) {
+      const getChars = CHARACTERS[cell];
+      divEls[index].innerText = getChars;
+    })
+    checkWinner();
   })
-
 };
 
-function spinReel() {
-  const randomNumbers = Math.floor((Math.random()* 100) + 1);
-  randomNumbers.innerText = "test";
-}
+function checkWinner() {
+  console.log(winArray[0])
+
+
+
+
+  // for(let i = 0; i < winningCombos.length; i++) {
+  //   let combo = winningCombos[i];
+  //   console.log(combo)
+  // } for(let j = 0; j < winningCombos.length; j++) {
+      
+  //     console.log()
+  // }
+
+
+
+  // console.log(combo[0], combo[2], combo[3], combo[4], combo[5], combo[6])
+  // const a = winArray[combo[0]];
+  // console.log(winArray)
+  // const b = winArray[combo[1]];
+  // const c = winArray[combo[2]];
+  // const d = winArray[combo[3]];
+  // const e = winArray[combo[4]];
+  // const f = winArray[combo[5]];
+  // const g = winArray[combo[6]];
+  // console.log(a,b,c,d,e,f,g)
+};
+
+
+// 1. SPIN BUTTON SHOULD AUTO GENERATE winArray
+  // 1.1 This needs to be available in the global scope for each function to read the winArray to perform their duty
+  // function spin() {
+  //   spinButtonEl.addEventListener('click', function(){
+  //     const winArray = Array.from({length: 15}, () =>Math.floor(Math.random() * 5) + 1);
+  //     return winArray;
+  //     console.log(winArray)
+  //   }
+  // }
+  // spin();
+// 2 AFTER SPIN IS CLICKED IT SHOULD BE READ BY renderScreen();
+  // 2.1 winArray should be converted to string of emojis (CHARACTERS{} object)
+
+
+// 3. CHECK winArray AFTER SCREEN HAS BEEN UPDATED WITH EMOJI'S BASED ON THEIR ARRAY CELL AND INDEX
+  // 3.1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // ICEBOX!!
